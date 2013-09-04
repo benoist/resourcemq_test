5ebcd4e65c29034ab0c2a3c48abcd0fe73d85e1d
Benoist <bclaassen@digidentity.eu>
2013-09-04 14:19:55 +0200

    WIP

--- server/app/messages/product_messages.rb
+++ server/app/messages/product_messages.rb
@@ -4,6 +4,14 @@ resource :product do
   attribute :price_in_cents, Integer
   attribute :published_at, Time
 
+  # Product Message
+  # {
+  #   name: 'name',
+  #   description: 'description',
+  #   price_in_cents: 100,
+  #   published_at: 12457
+  # }
+
   message :products do
     attribute :page, Integer
     attribute :total, Integer
@@ -11,10 +19,27 @@ resource :product do
     has_many :products, Product
   end
 
+  # Products Message
+  # {
+  #   page: 1,
+  #   total: 300,
+  #   products: [{
+  #     name: 'name',
+  #     description: 'description',
+  #     price_in_cents: 100,
+  #     published_at: 12457
+  #   }]
+  # }
+
   collection do
     action :index, responds_with: :products do
       params :page, Integer
     end
+
+    # Index message
+    # {
+    #   page: 1
+    # }
 
     action :create, responds_with: :product do
       attribute :name, String
@@ -22,6 +47,14 @@ resource :product do
       attribute :price_in_cents, Integer
       attribute :published_at, Time
     end
+
+    # Create message
+    # {
+    #   name: 'name',
+    #   description: 'description',
+    #   price_in_cents: 100,
+    #   published_at: 12457
+    # }
   end
 
   member do
@@ -31,8 +64,33 @@ resource :product do
       attribute :published_at, Time
     end
 
+    # Update message
+    # {
+    #   id: 1,
+    #   description: 'description',
+    #   price_in_cents: 100,
+    #   published_at: 12457
+    # }
+
     action :show, responds_with: :product
+
+    # Show message
+    # {
+    #   id: 1
+    # }
+
     action :destroy, responds_with: :product
+
+    # Publish message
+    # {
+    #   id: 1
+    # }
+
     action :publish, responds_with: :product
+    
+    # Publish message
+    # {
+    #   id: 1
+    # }
   end
 end
