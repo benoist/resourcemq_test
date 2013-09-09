1a743b5ebb0164750115735b7b8e60e3f3489000
Benoist <bclaassen@digidentity.eu>
2013-09-09 09:23:43 +0200

    WIP

--- server/app/messages/product_messages.rb
+++ server/app/messages/product_messages.rb
@@ -1,102 +1,104 @@
-resource :product do
-  attribute :name, String
-  attribute :description, String
-  attribute :price_in_cents, Integer
-  attribute :published_at, Time
-
-  # Product Message
-  # {
-  #   name: 'name',
-  #   description: 'description',
-  #   price_in_cents: 100,
-  #   published_at: 12457
-  # }
-
+service :profile_manager do
   header do
     param :trace_id, String
     param :remote_ip, String
     param :token, String
   end
 
-  collection_response :products do
-    # collection_of :product # this is default :products.singularize
+  resource :product do
+    attribute :name, String
+    attribute :description, String
+    attribute :price_in_cents, Integer
+    attribute :published_at, Time
 
-    attribute :page, Integer
-    attribute :total, Integer
-  end
-
-  # Products Message
-  # {
-  #   page: 1,
-  #   total: 300,
-  #   products: [{
-  #     name: 'name',
-  #     description: 'description',
-  #     price_in_cents: 100,
-  #     published_at: 12457
-  #   }]
-  # }
-
-  collection do
-    action :index, responds_with: :products do
-      param :page, Integer
-    end
-
-    # Index message
-    # {
-    #   page: 1
-    # }
-
-    action :create, responds_with: :product do
-      attribute :name, String
-      attribute :description, String
-      attribute :price_in_cents, Integer
-      attribute :published_at, Time
-    end
-
-    # Create message
+    # Product Message
     # {
     #   name: 'name',
     #   description: 'description',
     #   price_in_cents: 100,
     #   published_at: 12457
     # }
-  end
 
-  member do
-    action :update, responds_with: :product do
-      attribute :description, String
-      attribute :price_in_cents, Integer
-      attribute :published_at, Time
+    collection_response :products do
+      # collection_of :product # this is default :products.singularize
+
+      attribute :page, Integer
+      attribute :total, Integer
     end
 
-    # Update message
+    # Products Message
     # {
-    #   id: 1,
-    #   description: 'description',
-    #   price_in_cents: 100,
-    #   published_at: 12457
+    #   page: 1,
+    #   total: 300,
+    #   products: [{
+    #     name: 'name',
+    #     description: 'description',
+    #     price_in_cents: 100,
+    #     published_at: 12457
+    #   }]
     # }
 
-    action :show, responds_with: :product
+    collection do
+      action :index, responds_with: :products do
+        param :page, Integer
+      end
 
-    # Show message
-    # {
-    #   id: 1
-    # }
+      # Index message
+      # {
+      #   page: 1
+      # }
 
-    action :destroy, responds_with: :product
+      action :create, responds_with: :product do
+        attribute :name, String
+        attribute :description, String
+        attribute :price_in_cents, Integer
+        attribute :published_at, Time
+      end
 
-    # Publish message
-    # {
-    #   id: 1
-    # }
+      # Create message
+      # {
+      #   name: 'name',
+      #   description: 'description',
+      #   price_in_cents: 100,
+      #   published_at: 12457
+      # }
+    end
 
-    action :publish, responds_with: :product
+    member do
+      action :update, responds_with: :product do
+        attribute :description, String
+        attribute :price_in_cents, Integer
+        attribute :published_at, Time
+      end
 
-    # Publish message
-    # {
-    #   id: 1
-    # }
+      # Update message
+      # {
+      #   id: 1,
+      #   description: 'description',
+      #   price_in_cents: 100,
+      #   published_at: 12457
+      # }
+
+      action :show, responds_with: :product
+
+      # Show message
+      # {
+      #   id: 1
+      # }
+
+      action :destroy, responds_with: :product
+
+      # Publish message
+      # {
+      #   id: 1
+      # }
+
+      action :publish, responds_with: :product
+
+      # Publish message
+      # {
+      #   id: 1
+      # }
+    end
   end
 end
